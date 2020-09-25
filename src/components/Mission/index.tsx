492f963e23438c972bbde842126d695919835642
Hasham Vakani <hashamvakani@users.noreply.github.com>
2020-09-25 21:17:54 +0500

    Space-X

---
+++ src/components/Mission/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MissionContainer = ()=>{
+    return(
+        <div></div>
+    )
+}
+
+export default MissionContainer;
---
+++ src/components/Mission/query.ts
@@ -0,0 +1,25 @@
+// import gql from 'graphql-tag';
+
+// export const MissionsInfo = gql`
+// query MissionsInfo{
+//     launches{
+//       mission_name
+//       mission_id
+//       launch_success
+//       launch_year
+//     }
+//   }
+// `
+
+import gql from 'graphql-tag';
+
+export const MissionsInfo = gql`
+query MissionsInfo{
+    launches {
+      mission_name
+      mission_id
+      launch_success
+      launch_year
+    }
+  }
+` 
